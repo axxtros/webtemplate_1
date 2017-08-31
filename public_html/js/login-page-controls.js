b77b094976ff2f10a3bb061bb744d7e452c4117f
ghofer <gabor.hofer@frontsoft.hu>
2017-08-31 13:37:43 +0200

    v.0.23

--- public_html/js/login-page-controls.js
+++ public_html/js/login-page-controls.js
@@ -4,13 +4,20 @@
     Lapvezérlő függvények.
 */
 
-(function(){    
-    window.onscroll = function(){
-        var scrolled = $(window).scrollTop();
-        //$('.parallax').css('background-position-y', -(scrolled * 0.2) + 'px');
-    };
-})();
+/*
+ * Háttérkép parallax animációja, ha kell akkor itt programozd le.
+ * @returns {undefined}
+ */
+//(function(){    
+//    window.onscroll = function(){
+//        var scrolled = $(window).scrollTop();
+//        //$('.parallax').css('background-position-y', -(scrolled * 0.2) + 'px');
+//    };
+//})();
 
+/*
+ * A parallax weboldalon az adott tartalomrészhez scrollozik animáltan.
+ */
 function scrollToContent(anchorTag) {
-    $('html, body').animate({scrollTop: $( $.attr(anchorTag, 'href') ).offset().top}, 500);
+    $('html, body').animate({scrollTop: $( $.attr(anchorTag, 'href') ).offset().top - 60}, 500);    //a 60 a felső fix menüsor height miatt kell bele, a body top padding-ja (--header-width-g)!
 }
