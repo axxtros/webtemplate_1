c69e8661b957287cb6d534f8ad7a0fe6ad33f04a
ghofer <gabor.hofer@frontsoft.hu>
2017-09-01 15:57:26 +0200

    v.0.30 Az egyes parallax képek különálló görgetésének megoldása programozottan

--- public_html/js/login-page-controls.js
+++ public_html/js/login-page-controls.js
@@ -5,20 +5,32 @@
 */
 
 var scrolled;
+var parallax_image_y_1 = 0;         //az első parallax kép mindig 0
+var parallax_image_y_2 = 300;       //ezek viszonyszámok, fontos, hogy mekkora tartalom van felettük, és ha változik az oldal nagysága, akkor ezeket a számokat utánuk kell állítani,
+                                    //annak érdekében, hogy a görgetett parallax képek 'nagyjából' középen legyenek a görgetési tartalom fókuszában (képernyő közepében)
 
 function initLoginPage() {
     $('.first-parallax').css({'background-position-y':0+'px'});    
-    $('.second-parallax').css({'background-position-y':100+'px'});
+    $('.second-parallax').css({'background-position-y':parallax_image_y_2+'px'});
 }
 
 /*
- * Háttérkép parallax animációja, ha kell akkor itt programozd le.
+ * Háttérkép(ek) parallax animációja, ha kell akkor itt programozd le.
  * @returns {undefined}
  */
 (function(){    
     window.onscroll = function(){
         scrolled = $(window).scrollTop();
-        $('.parallax').css('background-position-y', -(scrolled * 0.2) + 'px');      //a 0.2 egy viszonyszám, amelyet az éppen alkalmazott képekhez kell igazítani
+        
+        //NE TÖRÖLD KI!!!
+        //ez az általános, amely mindegyik egyformán mozgatja
+//        $('.parallax').css('background-position-y', -(scrolled * 0.2) + 'px');      //a 0.2 egy viszonyszám, amelyet az éppen alkalmazott képekhez kell igazítani
+
+        var parallax_image_scroll_y_1 = parallax_image_y_1 - (scrolled * 0.2);
+        $('.first-parallax').css('background-position-y', parallax_image_scroll_y_1 + 'px');
+        
+        var parallax_image_scroll_y_2 = parallax_image_y_2 - (scrolled * 0.2);
+        $('.second-parallax').css('background-position-y', parallax_image_scroll_y_2 + 'px');        
     };
 })();
 
@@ -27,7 +39,7 @@ function initLoginPage() {
  */
 function scrollToContent(anchorTag) {    
     //ezt majd kezeld, mert le FF alatt nem jó
-    event.preventDefault();             //ne hívódjon meg az anchor default esemény, a lap tetejére ugrás (FF alatt nincs automatikusan felinicializálva az event!!!)
+    //event.preventDefault();             //ne hívódjon meg az anchor default esemény, a lap tetejére ugrás (FF alatt nincs automatikusan felinicializálva az event!!!)
     
     $('html, body').animate({scrollTop: $( $.attr(anchorTag, 'href') ).offset().top - 60}, 500);    //a 60 a felső fix menüsor height miatt kell bele, a body top padding-ja (css: --header-width-g)!        
     return false;
